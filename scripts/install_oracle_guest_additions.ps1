08d33b78ec467046496560e87816ad9cb91db7b8
Solid Ocean Trust <SolidOceanTrust@users.noreply.github.com>
2017-03-05 08:43:05 -0500

    Fix VBox Tools for New Virtualbox Versions
    * fixes oracle tools install
    
    updates install_oracle_guest_addtions.ps1 to reflect new cert names in vbox
    
    * adds future proofing for cert installs in vbox
    
    * Revert "adds future proofing for cert installs in vbox"
    
    This reverts commit 806ce9f16a3e321afcb5913954e53f1119631515.
    
    * Delete install_windows_updates_WSUSOffline.ps1
    
    * future proofs vbox guest installs. Resolves Issue #3

--- scripts/install_oracle_guest_additions.ps1
+++ scripts/install_oracle_guest_additions.ps1
@@ -1,7 +1,12 @@
 if ($env:install_vbox_tools -eq $true)
 {
   Write-Host "Installing Virtualbox Guest Additions"
-  certutil -addstore -f "TrustedPublisher" E:\cert\oracle-vbox.cer
+  Write-Host "Checking for Certificates in vBox ISO"
+  if(test-path E:\ -Filter *.cer)
+  {
+    Get-ChildItem E:\cert -Filter *.cer | ForEach-Object { certutil -addstore -f "TrustedPublisher" $_.FullName }
+  } 
+
   Start-Process -FilePath "E:\VBoxWindowsAdditions.exe" -ArgumentList "/S" -Wait
 }
 else
