8f8ecb1d14908e46291e38c87028db3c3ee16c9c
Matthew Hodgkins <matthodge@gmail.com>
2017-10-23 08:24:51 +0200

    Fix Windows Update Script

--- scripts/install_windows_updates.ps1
+++ scripts/install_windows_updates.ps1
@@ -2,7 +2,7 @@ $wuInstallExe = Join-Path "$($env:windir)\SYSTEM32" 'WUInstallAMD64.exe'
 
 if (!(Test-Path -Path $wuInstallExe))
 {
-    Invoke-WebRequest -UseBasicParsing -Uri 'https://dl.dropboxusercontent.com/u/727435/Tools/WUInstallAMD64.exe' -OutFile $wuInstallExe
+    Invoke-WebRequest -UseBasicParsing -Uri 'https://www.dropbox.com/s/u0cf68kjw1i00n5/WUInstallAMD64.exe?dl=1' -OutFile $wuInstallExe
 }
 
 C:\WINDOWS\SYSTEM32\WUInstallAMD64.exe /install /autoaccepteula /silent
